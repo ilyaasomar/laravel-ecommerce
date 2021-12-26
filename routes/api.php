aac667f34d5289485a22d936802a4339c82b1e86
unknown <ilyaascumar10@gmail.com>
2021-12-26 16:35:42 +0300

    ecommerce backend refers to uper repo with react js

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+use Facade\FlareClient\Http\Response;
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::get("/checkingAuthonticated", function(){
+        return response()->json(['message' => 'You are in', 'status' => 200], 200);
+    });
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
