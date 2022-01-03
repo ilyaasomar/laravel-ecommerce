882405c85db4945979c851160fec42b7e1e2fc61
unknown <ilyaascumar10@gmail.com>
2022-01-03 11:51:17 +0300

    complete category

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CategoryController;
+
 use Facade\FlareClient\Http\Response;
 
 Route::post('register', [AuthController::class, 'register']);
@@ -14,6 +16,12 @@ Route::middleware(['auth:sanctum','isAPIAdmin'])->group(function(){
     Route::get("/checkingAuthonticated", function(){
         return response()->json(['message' => 'You are in', 'status' => 200], 200);
     });
+    Route::get('view-category',[CategoryController::class,'index']);
+    Route::post('store-category',[CategoryController::class,'store']);
+    Route::get('edit-category/{id}',[CategoryController::class,'edit']);
+    Route::put('update-category/{id}',[CategoryController::class,'update']);
+    Route::delete('delete-category/{id}',[CategoryController::class,'destroy']);
+
   
 });
 Route::middleware(['auth:sanctum'])->group(function(){
